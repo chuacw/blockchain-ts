00713ba546437bfadf5d8d4ab242c208d24b8c90
Chua Chee Wee <1757930+chuacw@users.noreply.github.com>
2020-12-21 18:10:40 +0800

    78
    78

---
+++ wallet/index.js
@@ -0,0 +1,26 @@
+// 78
+
+const ChainUtil = require('../chain-util');
+const { INITIAL_BALANCE } = require('../config');
+class Wallet {
+    /**
+    * the wallet will hold the public key
+    * and the private key pair
+    * and the balance
+    */
+    constructor() {
+        this.balance = INITIAL_BALANCE;
+        this.keyPair = ChainUtil.genKeyPair();
+        this.publicKey = this.keyPair.getPublic().encode('hex');
+    }
+
+    toString() {
+        return `Wallet - publicKey: ${this.publicKey.toString()} balance : ${this.balance}`
+    }
+
+    sign(dataHash) {
+        return this.keyPair.sign(dataHash);
+    }
+
+}
+module.exports = Wallet;
