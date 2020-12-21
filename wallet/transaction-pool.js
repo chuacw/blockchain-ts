20b1365f4b4ca64c880c1658d698f88137d79758
Chua Chee Wee <1757930+chuacw@users.noreply.github.com>
2020-12-21 18:25:57 +0800

    90
    90

---
+++ wallet/transaction-pool.js
@@ -0,0 +1,35 @@
+// 90
+
+const Transaction = require('./transaction');
+class TransactionPool {
+
+    constructor() {
+        // represents a collections of transactions in the pool
+        this.transactions = [];
+    }
+
+    /**
+    * this method will add a transaction
+    * it is possible that the transaction exists already
+    * so it will replace the transaction with the new transaction
+    * after checking the input id and adding new outputs if any
+    * we call this method and replace the transaction in the pool
+    */
+    updateOrAddTransaction(transaction) {
+        // get the transaction while checking if it exists
+        let transactionWithId = this.transactions.find(t => t.id ===
+            transaction.id);
+        if (transactionWithId) {
+            this.transactions[this.transactions.indexOf(transactionWithId)]
+                = transaction;
+        } else {
+            this.transactions.push(transaction);
+        }
+    }
+
+    existingTransaction(address) {
+        return this.transactions.find(t => t.input.address === address);
+    }
+    
+}
+module.exports = TransactionPool;
