363b775f1461f4627ab17005f21f72f83700ae78
Chua Chee Wee <1757930+chuacw@users.noreply.github.com>
2020-12-21 18:08:58 +0800

    77
    77

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
+// 74
+const INITIAL_BALANCE = 500;
+
 const DIFFICULTY = 4;
 const MINE_RATE = 3000;
 module.exports = {
     DIFFICULTY,
-    MINE_RATE
+    MINE_RATE,
+    INITIAL_BALANCE // 74
 }
