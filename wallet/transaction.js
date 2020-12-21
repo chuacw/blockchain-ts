6011f9ec5bfa738fbf1c886cce32164056b5317a
Chua Chee Wee <1757930+chuacw@users.noreply.github.com>
2020-12-21 18:15:16 +0800

    82
    82

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -7,6 +7,24 @@ class Transaction {
         this.outputs = [];
     }
 
+    // 82
+    static newTransaction(senderWallet, recipient, amount) {
+        if (amount > senderWallet.balance) {
+            console.log(`Amount : ${amount} exceeds the balance`);
+            return;
+        } 
+        const transaction = new this();
+        transaction.outputs.push(...[
+            {
+                amount: senderWallet.balance - amount, address:
+                    senderWallet.publicKey
+            },
+            { amount: amount, address: recipient }
+        ]);
+        Transaction.signTransaction(transaction, senderWallet);
+        return transaction;
+    }
+
 }
 
 module.exports = Transaction;
