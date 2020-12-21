64ec41597b88c575dea15a151d1485c099ade4f0
Chua Chee Wee <1757930+chuacw@users.noreply.github.com>
2020-12-21 19:04:16 +0800

    129
    129

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -58,6 +58,14 @@ class Transaction {
         return this;
     }
 
+    // 129
+    static transactionWithOutputs(senderWallet, outputs) {
+        const transaction = new this();
+        transaction.outputs.push(...outputs);
+        Transaction.signTransaction(transaction, senderWallet);
+        return transaction;
+    }
+
 }
 
 module.exports = Transaction;
