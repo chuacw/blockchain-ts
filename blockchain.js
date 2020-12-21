3c53e8a5336b312a9f0cb2a0b5bb6353b9b48eff
Chua Chee Wee <1757930+chuacw@users.noreply.github.com>
2020-12-21 16:43:37 +0800

    20
    20

--- blockchain.js
+++ blockchain.js
@@ -4,6 +4,15 @@ class Blockchain {
     constructor() {
         this.chain = [Block.genesis()];
     }
+
+    // 20
+    addBlock(data) {
+        const block = Block.mineBlock(this.chain[this.chain.length - 1],
+            data);
+        this.chain.push(block);
+        return block;
+    }
+
 }
 
 module.exports = Blockchain;
