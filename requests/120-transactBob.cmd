229f05f9646e287cf31e67723ee4d1c80aecf6fb
Chua Chee Wee <1757930+chuacw@users.noreply.github.com>
2020-12-21 18:58:10 +0800

    122
    122

---
+++ requests/120-transactBob.cmd
@@ -0,0 +1,1 @@
+curl -X POST --data @120-transactBob.json -H "Content-Type: application/json" http://localhost:3001/transact
--- requests/120transactbob.cmd
+++
@@ -1,1 +0,0 @@
-curl -X POST --data @120transactBob.json -H "Content-Type: application/json" http://localhost:3001/transact
