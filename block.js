02500033ed104b430317d50985128c905fa5e751
Chua Chee Wee <1757930+chuacw@users.noreply.github.com>
2020-12-21 16:56:41 +0800

    Revert "26"
    This reverts commit 0c01e00ae32daa9f0bb8a89633bc93182b370301.

--- block.js
+++ block.js
@@ -59,18 +59,5 @@ class Block {
         return true;
     }
 
-    // 26
-    replaceChain(newChain) {
-        if (newChain.length <= this.chain.length) {
-            console.log("Received chain is no longer than the current chain");
-            return;
-        } else if (!this.isValidChain(newChain)) {
-            console.log("Received chain is invalid");
-            return;
-        }
-        console.log("Replacing the current chain with new chain");
-        this.chain = newChain;
-    }
-
 }
 module.exports = Block;
