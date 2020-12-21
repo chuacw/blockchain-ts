054006bbbbe3988b371891d4c5cd41b0dfdd5022
Chua Chee Wee <1757930+chuacw@users.noreply.github.com>
2020-12-21 16:58:24 +0800

    25 and 26
    25 and 26

--- blockchain.js
+++ blockchain.js
@@ -13,6 +13,33 @@ class Blockchain {
         return block;
     }
 
+    // 25
+    isValidChain(chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
+            return false;
+        for (let i = 1; i < chain.length; i++) {
+            const block = chain[i];
+            const lastBlock = chain[i - 1];
+            if ((block.lastHash !== lastBlock.hash) || (block.hash !==
+                Block.blockHash(block)))
+                return false;
+        }
+        return true;
+    }
+
+    // 26
+    replaceChain(newChain) {
+        if (newChain.length <= this.chain.length) {
+            console.log("Received chain is no longer than the current chain");
+            return;
+        } else if (!this.isValidChain(newChain)) {
+            console.log("Received chain is invalid");
+            return;
+        }
+        console.log("Replacing the current chain with new chain");
+        this.chain = newChain;
+    }
+
 }
 
 module.exports = Blockchain;
